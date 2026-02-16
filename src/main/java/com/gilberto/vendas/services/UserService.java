b4035711f822bcd85467b63b2f68cc9d8aa5ecd2
Gilberto <gilberto.ribeiro10@hotmail.com>
2026-02-16 18:06:16 -0300

    Service layer, component registration

---
+++ src/main/java/com/gilberto/vendas/services/UserService.java
@@ -0,0 +1,26 @@
+package com.gilberto.vendas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilberto.vendas.entities.User;
+import com.gilberto.vendas.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
