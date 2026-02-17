7b774cbc557848b68950b7f021b920d821ad4f4f
Gilberto <gilberto.ribeiro10@hotmail.com>
2026-02-16 23:27:48 -0300

    Category

---
+++ src/main/java/com/gilberto/vendas/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.gilberto.vendas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gilberto.vendas.entities.Category;
+import com.gilberto.vendas.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
