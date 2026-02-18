f0ad06f66270f1ca2137dcb80cd2444606c312c5
Gilberto <gilberto.ribeiro10@hotmail.com>
2026-02-18 18:46:00 -0300

    Payment, one-to-one association

--- src/main/java/com/gilberto/vendas/config/TestConfig.java
+++ src/main/java/com/gilberto/vendas/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.gilberto.vendas.entities.Category;
 import com.gilberto.vendas.entities.Order;
 import com.gilberto.vendas.entities.OrderItem;
+import com.gilberto.vendas.entities.Payment;
 import com.gilberto.vendas.entities.Product;
 import com.gilberto.vendas.entities.User;
 import com.gilberto.vendas.entities.enums.OrderStatus;
@@ -82,5 +83,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
